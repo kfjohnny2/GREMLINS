2a49e8cf2cff88bcba081fd2fbed465e065ac125
kfjohnny2 <kfjohnny@ufrn.edu.br>
2016-05-24 13:41:49 +0000

    First commit

---
+++ include/slpool.hpp
@@ -0,0 +1,28 @@
+c l a s s SLPool : p u b l i c StoragePool {
+
+public :
+
+struct Header {
+unsigned int mui_Length ;
+Header(): mui_Length (0u ) { /* Empty */ };
+};
+struct Block : public Header {
+enum { BlockSize = 16 }; // Each block has 16 bytes .
+union {
+Block *mp_Next ;
+char mc_RawArea [ BlockSize - sizeof( Header ) ];
+};
+Block(): Header (), mp_Next ( nullptr ) { /* Empty */ };
+};
+private:
+unsigned int mui_NumberOfBlocks ;
+Block *mp_Pool ;
+// ! < Head of list .
+Block &mr_Sentinel ;
+// ! < End of the list .
+public :
+explicit SLPool ( size_t );
+~SLPool ();
+void * Allocate ( size_t );
+void Free ( void * );
+};
