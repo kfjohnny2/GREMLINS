3c87cbed308f5c7cbec4e2dd7224b6a3843e05c0
kfjohnny2 <kfjohnny@ufrn.edu.br>
2016-06-09 01:23:17 +0000

    Alterations on  readme file

--- include/allocate.hpp
+++ include/allocate.hpp
@@ -1,12 +1,3 @@
-/**
- *  @file allocate.hpp
- *  @brief GREMLINS Implementation
- *  @author Johnnylee Bryan Marques da Rocha e Pedro Arthur Medeiros Fernandes.
- *  @copyright Copyright &copy; 2016. All rights reserved.
- * 
- *  File with the declarations of new operators.
- */
- 
 #include "storagepool.hpp"
 #ifndef _ALLOCATE_H_
 #define _ALLOCATE_H_
